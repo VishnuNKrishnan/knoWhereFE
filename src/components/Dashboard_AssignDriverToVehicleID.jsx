7e7cc49b7b2108e495f55d53bfd19b90825c013e
VishnuNKrishnan <navaneeth.vishnu1@gmail.com>
2022-07-23 12:36:47 +0400

    API endpoints corrected

--- src/components/Dashboard_AssignDriverToVehicleID.jsx
+++ src/components/Dashboard_AssignDriverToVehicleID.jsx
@@ -54,13 +54,16 @@ function Dashboard_AssignDriverToVehicleID() {
       },
     }
     const serverResponse = await fetch(
-      // `${process.env.REACT_APP_API_SERVER_BASE_URL}/app/getOTP`,
-      `http://192.168.0.150:3001/app/getOTP`,
+      `${process.env.REACT_APP_API_SERVER_BASE_URL}/app/getOTP`,
+      // `http://192.168.0.150:3001/app/getOTP`,
       // `http://nvmservices.ddns.net:3001/app/getOTP`,
       options,
     ).catch((err) => console.log(err))
     const serverResponseData = await serverResponse.json()
-    console.log(serverResponseData)
+    //console.log(serverResponseData)
+    serverResponseData.success === true
+      ? setRequestId(serverResponseData.requestId)
+      : null
   }
 
   //Function to call API to upload and update new driver details
@@ -76,8 +79,8 @@ function Dashboard_AssignDriverToVehicleID() {
       },
     }
     const serverResponse = await fetch(
-      // `${process.env.REACT_APP_API_SERVER_BASE_URL}/app/assignDriverToVehicleID`,
-      `http://192.168.0.150:3001/app/assignDriverToVehicleID`,
+      `${process.env.REACT_APP_API_SERVER_BASE_URL}/app/assignDriverToVehicleID`,
+      // `http://192.168.0.150:3001/app/assignDriverToVehicleID`,
       // `http://nvmservices.ddns.net:3001/app/assignDriverToVehicleID`,
       options,
     ).catch((err) => console.log(err))
